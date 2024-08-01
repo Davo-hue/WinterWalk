7a79a564ae32f4860cb1d816fa13d63c02557b05
Davo-hue <dnowland@myjmc.edu.au>
2024-08-01 15:00:38 +1000

    PlayerMovement and Camera

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    public Rigidbody rb;
+    public SpriteRenderer sr;
+
+    
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        Vector3 moveDir = new Vector3(x, 0, 0);
+        rb.linearVelocity = moveDir * speed;  
+
+       
+    }
+}
